724252675227622d44450e69b348932f15769daf
arndt-k <90337376+arndt-k@users.noreply.github.com>
2025-04-04 13:02:18 +0200

    search

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 ingredient_list = st.multiselect('Choose up to 5:', my_dataframe,
                                 max_selections=6)
 if ingredient_list:
@@ -33,6 +33,9 @@ if ingredient_list:
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
         
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=True)
     st.write(ingredients_string)
