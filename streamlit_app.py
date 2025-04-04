9e70363cc0deb3f65f0005d5e905a41a5e3a1f5b
arndt-k <90337376+arndt-k@users.noreply.github.com>
2025-04-04 12:58:48 +0200

    search

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ st.write('the name will be ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredient_list = st.multiselect('Choose up to 5:', my_dataframe,
                                 max_selections=6)
 if ingredient_list:
